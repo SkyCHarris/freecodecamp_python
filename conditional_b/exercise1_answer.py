365faf2ae5a72eff45279244ae95359a7921c171
Sky Harris <skyler.iqeq@gmail.com>
2023-08-22 22:55:04 -0600

    Finalized and cleaned exercise 1 answer file

--- conditional_b/exercise1_answer.py
+++ conditional_b/exercise1_answer.py
@@ -1,12 +1,8 @@
 hours = float(input('Enter Hours:'))
 rate = float(input('Enter Rate:'))
 reg_pay = hours * rate
-""" print(hours, rate)
-pay = hours * rate
-print("Pay:", pay) """
 
 if hours > 40 :
-    print("Overtime")
     over_pay = (hours - 40.0) * (rate * 0.5)
     print(reg_pay + over_pay)
 else :
