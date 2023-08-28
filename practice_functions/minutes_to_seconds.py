193147f2b9b3ae91ce19c95b13caa0e8b0bf84df
SkyCHarris <skyler.iqeq@gmail.com>
2023-08-27 23:37:53 -0600

    convert minutes to seconds problem

---
+++ practice_functions/minutes_to_seconds.py
@@ -0,0 +1,9 @@
+# Write a function that takes an integer 'minutes' and converts it to seconds
+
+def convert(minutes) :
+    seconds = minutes * 60
+    print(seconds)
+    return seconds
+  
+
+convert(5)
